7a8825ecaa5a0750dca930190583a0d421d9486d
suhyun2963 <k40001338@gmail.com>
2020-07-13 11:02:46 +0900

    Trend, Article, Coupon -fragment 삽입

--- app/src/main/java/com/suek/nyamnyam/FragmentTab3Article.java
+++ app/src/main/java/com/suek/nyamnyam/FragmentTab3Article.java
@@ -8,13 +8,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentTab3Article extends Fragment {
+
+    ArrayList<ArticleItem> articleItems= new ArrayList<>();
+    ArticleAdapter articleAdapter;
+    RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view= inflater.inflate(R.layout.tab3_article_fragment, container, false);
+
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+        articleItems.add(new ArticleItem("https://esculapio.pk/wp-content/uploads/2018/07/article-submission.jpg", "5 reasons why Korean food is healthy"));
+
+
+        recyclerView= view.findViewById(R.id.recycler);
+        articleAdapter= new ArticleAdapter(view.getContext(), articleItems);
+        recyclerView.setAdapter(articleAdapter);
+
         return view;
     }
 }
--- app/src/main/java/com/suek/nyamnyam/FragmentTab4Coupon.java
+++ app/src/main/java/com/suek/nyamnyam/FragmentTab4Coupon.java
@@ -8,13 +8,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentTab4Coupon extends Fragment {
+
+    ArrayList<CouponItem> couponItems= new ArrayList<>();
+    CouponAdapter adapter;
+    RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view= inflater.inflate(R.layout.tab4_coupon_fragment, container, false);
+
+
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+        couponItems.add(new CouponItem("https://freepngimg.com/thumb/web_design/51726-5-coupon-picture-free-hq-image.png","G market 50% discount", "2020-10-20"));
+
+
+        recyclerView= view.findViewById(R.id.recycler);
+        adapter= new CouponAdapter(view.getContext(), couponItems);
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
 }
