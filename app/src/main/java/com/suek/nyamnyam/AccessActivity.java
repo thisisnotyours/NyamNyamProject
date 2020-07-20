666a17e3d382a6d4834ee062663d826f6df9b4c8
suhyun2963 <k40001338@gmail.com>
2020-07-20 10:52:53 +0900

    Google Login 기능 연결

--- app/src/main/java/com/suek/nyamnyam/AccessActivity.java
+++ app/src/main/java/com/suek/nyamnyam/AccessActivity.java
@@ -7,25 +7,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccessActivity extends AppCompatActivity {
+
+    private TextView tvNickname;  //받아온 닉네임값
+    private CircleImageView civ;   //받아온 프로필 이미지값
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access);
 
-        //handler.sendEmptyMessageDelayed(0, 3000);
+        handler.sendEmptyMessageDelayed(0, 2000);
+
+        Intent intent= getIntent();
+        String nickName= intent.getStringExtra("nickName");
+        String profileUrl= intent.getStringExtra("profileUrl");
+
+        tvNickname= findViewById(R.id.tv_nickname);
+        tvNickname.setText(nickName);    //닉네임- 텍스트뷰에 세팅
+
+        civ= findViewById(R.id.civ);
+        Glide.with(this).load(profileUrl).into(civ);   //포토Url- Civ 뷰에 세팅
+
+
 
     }//onCreate..
 
-    /*Handler handler= new Handler(){
+    Handler handler= new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg) {
             Intent intent= new Intent(AccessActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }
-    };*/
+    };
 
 }//Access Activity..
--- app/src/main/java/com/suek/nyamnyam/LoginActivity.java
+++ app/src/main/java/com/suek/nyamnyam/LoginActivity.java
@@ -1,14 +1,38 @@
 package com.suek.nyamnyam;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private SignInButton btn_google;   //구글 로그인버튼
+    private FirebaseAuth auth;         //파이어베이스 인증 객체
+    private GoogleApiClient googleApiClient;   //구글 ApiClient 객체
+    private static final int REQ_SIGN_GOOGLE= 100;  //구글 로그인했을때 결과코드
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +41,70 @@ public class LoginActivity extends AppCompatActivity {
 
         //handler.sendEmptyMessageDelayed(0, 3000);
 
+        //사인버튼을 누르면 기본적으로 세팅되는 옵션들..
+        GoogleSignInOptions googleSignInOptions= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        googleApiClient= new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();
+
+        auth= FirebaseAuth.getInstance();    //파이어베이스 인증객체 초기화
+
+
+        btn_google= findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= Auth.GoogleSignInApi.getSignInIntent(googleApiClient);   //구글 인증 액티비티(구글이 만든 다른 화면으로)으로 이동
+                startActivityForResult(intent, REQ_SIGN_GOOGLE);
+            }
+        });
 
     }//onCreate..
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQ_SIGN_GOOGLE){
+            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account= result.getSignInAccount();  //account= 구글로그인 정보를 담고있음(닉네임, 프로필사진, 이메일주소..etc)
+                resultLogin(account);  //로그인 결과값 실행하는 메소드
+            }
+        }
+
+    }
+
+
+    //로그인 인증 결과
+    private void resultLogin(GoogleSignInAccount account) {      //account= 구글로그인 정보를 담고있음(닉네임, 프로필사진, 이메일주소..etc)
+        AuthCredential credential= GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {     //로그인 인증결과
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                    Intent intent= new Intent(getApplicationContext(), AccessActivity.class);
+                    intent.putExtra("nickName", account.getDisplayName());        //구글에서 지정한 닉네임을 가져올수있음
+                    intent.putExtra("profileUrl", String.valueOf( account.getPhotoUrl()));  //구글에서 지정한 프로필 사진을 가져올수있음- 특정자료형을 String 형태로 변환시키는 형태
+                    startActivity(intent);
+
+                }else {
+                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 
     /*Handler handler= new Handler(){
         @Override
